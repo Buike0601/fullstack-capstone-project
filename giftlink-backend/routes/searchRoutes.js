95c4e2d4a4570e63ee54f109439dd8728d82d64e
Buike <buikenduaguba@gmail.com>
2025-10-28 11:55:35 -0400

    create API for search

--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -14,9 +14,9 @@ router.get('/', async (req, res, next) => {
         let query = {};
 
         // Add the name filter to the query if the name parameter is not empty
-        // if (req.query.name>req.query.name && req.query.name.trim() !== '') {
+        if (req.query.name>req.query.name && req.query.name.trim() !== '') {
             query.name = { $regex: req.query.name, $options: "i" }; // Using regex for partial match, case-insensitive
-        // }
+        }
 
         // Task 3: Add other filters to the query
         if (req.query.category) {
@@ -26,7 +26,6 @@ router.get('/', async (req, res, next) => {
             query.condition = req.query.condition;
         }
         if (req.query.age_years) {
-            // {{insert code here}}
             query.age_years = { $lte: parseInt(req.query.age_years) };
         }
 
