a2e8b11a575ab7b772fb714a4e34db691eb37265
Buike0601 <buikenduaguba@gmail.com>
2025-10-29 14:33:42 -0400

    Finish Adding Front-end Pages

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
-import RegisterPage from '/components/RegisterPage/RegisterPage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
@@ -19,7 +19,7 @@ function App() {
                 <Route path="/" element={<MainPage />} />
                 <Route path="/app" element={<MainPage />} />
                 <Route path="/app/login" element={<LoginPage />} />
-                <Route path="/app/login" element={<RegisterPage />} />
+                <Route path="/app/register" element={<RegisterPage />} />
                 <Route path="/app/search" element={SearchPage} />
             </Routes>
         </>
