3dd28038dfd9376502000945c1349a8f69abb22f
Buike0601 <buikenduaguba@gmail.com>
2025-10-29 14:34:48 -0400

    Finish Adding Front-end Pages

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,4 +1,3 @@
-/*jshint esversion: 8 */
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -36,10 +35,10 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 // Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-app.use('./api/gifts', giftRoutes);
+app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-app.use('./api/search', searchRoutes);
+app.use('/api/search', searchRoutes);
 
 
 // Global Error Handler
