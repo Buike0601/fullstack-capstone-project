6fe6cb21e8648e0a9ace435bcc999db6fce56ac0
Buike <buikenduaguba@gmail.com>
2025-10-30 13:17:10 -0400

    Implement Back-end in Login Component

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import { urlConfig } from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
 import './LoginPage.css';
 
 function LoginPage() {
@@ -6,9 +10,49 @@ function LoginPage() {
     //insert code here to create useState hook variables for email, password
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [wrongPass, setWrongPass] = useState('');
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
+
+    useEffect(() => {
+        if (sessionStorage.getItem('auth-token')) {
+            navigate('/app')
+        }
+    }, [navigate])
+
     // insert code here to create handleLogin function and include console.log
-    const handleLogin = async () => {
-        console.log("Inside handleLogin");
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        const res = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': bearerToken ? `Bearer ${bearerToken}` : '',
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            })
+        });
+
+        const json = await res.json();
+        console.log('Json', json);
+        if (json.authtoken) {
+            sessionStorage.setItem('auth-token', json.authtoken);
+            sessionStorage.setItem('name', json.userName);
+            sessionStorage.setItem('email', json.userEmail);
+            setIsLoggedIn(true);
+            navigate('/app');
+        } else {
+            document.getElementById("email").value = "";
+            document.getElementById("password").value = "";
+            setIncorrect("Wrong password. Try again.");
+            setTimeout(() => {
+                setIncorrect("");
+            }, 2000);
+        }
+
     }
     return (
         <div className="container mt-5">
@@ -16,7 +60,6 @@ function LoginPage() {
                 <div className="col-md-6 col-lg-4">
                     <div className="login-card p-4 border rounded">
                         <h2 className="text-center mb-4 font-weight-bold">Login</h2>
-
                         {/* insert code here to create input elements for the variables email and  password */}
                         <div className="mb-3">
                             <label htmlFor="email" className="form-label">Email</label>
@@ -26,7 +69,7 @@ function LoginPage() {
                                 className="form-control"
                                 placeholder="Enter your email"
                                 value={email}
-                                onChange={(e) => setEmail(e.target.value)}
+                                onChange={(e) => { setEmail(e.target.value); setIncorrect("") }}
                             />
                         </div>
 
@@ -38,8 +81,9 @@ function LoginPage() {
                                 className="form-control"
                                 placeholder="Enter your password"
                                 value={password}
-                                onChange={(e) => setPassword(e.target.value)}
+                                onChange={(e) => { setPassword(e.target.value); setIncorrect("") }}
                             />
+                            <span style={{ color: 'red', height: '.5cm', display: 'block', fontStyle: 'italic', fontSize: '12px' }}>{incorrect}</span>
                         </div>
                         {/* insert code here to create a button that performs the `handleLogin` function on click */}
                         <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
